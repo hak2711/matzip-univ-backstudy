aa550958626627c9df65d7a5b7f2be5c8f2cfb3a
hyunaaaa <67725652+hak2711@users.noreply.github.com>
2021-06-28 10:00:56 +0900

    Update LoginUserArgumentResolver.java

--- socialLogin/config/auth/LoginUserArgumentResolver.java
+++ socialLogin/config/auth/LoginUserArgumentResolver.java
@@ -17,17 +17,18 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
     private final HttpSession httpSession;
 
     @Override
-    public boolean supportsParameter(MethodParameter parameter) {
+    public boolean supportsParameter(MethodParameter parameter) { // 컨트롤러 메서드의 특정 파라미터를 지원하는지 판단.
         boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
         boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
         return isLoginUserAnnotation && isUserClass;
     }
 
     @Override
+    // 파라미터에 전달할 객체 생성
     public Object resolveArgument(MethodParameter parameter,
                                   ModelAndViewContainer mavContainer,
                                   NativeWebRequest webRequest,
                                   WebDataBinderFactory binderFactory) throws Exception {
-        return httpSession.getAttribute("user");
+        return httpSession.getAttribute("user"); // 세션에서 객체 가져옴
     }
 }
