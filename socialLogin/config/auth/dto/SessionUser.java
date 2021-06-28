7cbeb7f34d5726591481b5642c17f1881a2ec431
hyunaaaa <67725652+hak2711@users.noreply.github.com>
2021-06-28 09:56:02 +0900

    Update SessionUser.java

--- socialLogin/config/auth/dto/SessionUser.java
+++ socialLogin/config/auth/dto/SessionUser.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import java.io.Serializable;
 
 @Getter
+//엔터티 클래스 자체를 직렬화하면 해당 엔터티와 관계를 가지고 있는 다른 엔터티까지도 직렬화 대상에 포함되므로
+//직렬화 기능을 가진 세션 dto를 추가로 생성
 public class SessionUser implements Serializable {
     private String name;
     private String email;
